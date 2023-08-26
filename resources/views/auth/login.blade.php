fdb12e84e9228aafa3098896a961b62965aa3fa2
Mramkk <rramkumarbedia@gmail.com>
2023-08-26 17:30:56 +0530

    new private check

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -31,7 +31,7 @@
 
                         <div class="mt-4 text-center">
 
-                            <h4>Log In KK.</h4>
+                            <h4>Log In.</h4>
                             <span>Login with your credentials</span>
 
                             <div class="mb-3 mt-3">
